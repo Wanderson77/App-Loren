381a46b025702235b42e5d39e54312999de64d0c
Wanderson77 <wanderson.aldo@gmail.com>
2023-06-23 13:21:17 -0300

    Big Feature

---
+++ src/screens/BigFeatScreen/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const index = () => {
+  handleBigFeature(){}
+  return (
+    <View>
+      <Text onPress={handleBigFeature}>index</Text>
+
+    </View>
+  )
+}
+
+export default index
